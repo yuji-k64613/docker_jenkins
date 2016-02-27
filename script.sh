4d0ad853a164e152268def6e88013a0b9ef9a8be
CoreOS Admin <you@example.com>
2016-02-27 08:37:00 +0000

    foo

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 grep '^trap' ~/.bashrc > /dev/null
 if [ $? -ne 0 ]; then
 cat << EOF >> ~/.bashrc
-trap 'pkill -TERM jenkins; sleep 3; exit 0' TERM
+trap '/etc/init.d/jenkins stop; sleep 3; exit 0' TERM
 EOF
 fi
 /etc/init.d/jenkins start
